922fb7ccc5f4c5ae470ba5e268391104fc9c3a38
MrPier9 <mrpier9@gmail.com>
2025-12-21 16:00:21 +0100

    end of sec 2

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -1,3 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var keycloak = builder.AddKeycloak("keycloak", 6001)
+    .WithDataVolume("keycloak-data")
+    .WithEnvironment("KC_HTTP_ENABLED", "true")
+    .WithEnvironment("KC_HOSTNAME_STRICT", "false");
+
 builder.Build().Run();

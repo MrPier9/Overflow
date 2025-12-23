f6946af02012a7d27006248f90a3cbf158e606de
MrPier9 <mrpier9@gmail.com>
2025-12-23 17:03:38 +0100

    end of sec 3

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -5,4 +5,16 @@ var keycloak = builder.AddKeycloak("keycloak", 6001)
     .WithEnvironment("KC_HTTP_ENABLED", "true")
     .WithEnvironment("KC_HOSTNAME_STRICT", "false");
 
+var postgres = builder.AddPostgres("postgres", port: 5432)
+    .WithDataVolume("postgres-data")
+    .WithPgAdmin();
+
+var questionDb = postgres.AddDatabase("questionDb");
+
+var questionService = builder.AddProject<Projects.QuestionService>("question-svc")
+    .WithReference(keycloak)
+    .WithReference(questionDb)
+    .WaitFor(keycloak)
+    .WaitFor(questionDb);
+
 builder.Build().Run();
